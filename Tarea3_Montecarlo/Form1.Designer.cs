93fc791d255bc2061efa7e526884e139aeeef01d
pedro <pedro@LAPTOP-DMRF6SEK>
2023-10-15 22:49:28 -0600

    Add project files.

---
+++ Tarea3_Montecarlo/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Tarea3_Montecarlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.experimentosTextBox = new System.Windows.Forms.TextBox();
+            this.cantidadTextBox = new System.Windows.Forms.TextBox();
+            this.minTextBox = new System.Windows.Forms.TextBox();
+            this.maxTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.stdDevLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número de experimentos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de paneles";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor mínimo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Valor máximo";
+            // 
+            // experimentosTextBox
+            // 
+            this.experimentosTextBox.Location = new System.Drawing.Point(238, 62);
+            this.experimentosTextBox.Name = "experimentosTextBox";
+            this.experimentosTextBox.Size = new System.Drawing.Size(100, 22);
+            this.experimentosTextBox.TabIndex = 4;
+            // 
+            // cantidadTextBox
+            // 
+            this.cantidadTextBox.Location = new System.Drawing.Point(238, 116);
+            this.cantidadTextBox.Name = "cantidadTextBox";
+            this.cantidadTextBox.Size = new System.Drawing.Size(100, 22);
+            this.cantidadTextBox.TabIndex = 5;
+            // 
+            // minTextBox
+            // 
+            this.minTextBox.Location = new System.Drawing.Point(238, 175);
+            this.minTextBox.Name = "minTextBox";
+            this.minTextBox.Size = new System.Drawing.Size(100, 22);
+            this.minTextBox.TabIndex = 6;
+            // 
+            // maxTextBox
+            // 
+            this.maxTextBox.Location = new System.Drawing.Point(238, 226);
+            this.maxTextBox.Name = "maxTextBox";
+            this.maxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.maxTextBox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(397, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(359, 281);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(397, 368);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(147, 16);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.Text = "Promedio Monte Carlo: ";
+            // 
+            // stdDevLabel
+            // 
+            this.stdDevLabel.AutoSize = true;
+            this.stdDevLabel.Location = new System.Drawing.Point(397, 399);
+            this.stdDevLabel.Name = "stdDevLabel";
+            this.stdDevLabel.Size = new System.Drawing.Size(134, 16);
+            this.stdDevLabel.TabIndex = 11;
+            this.stdDevLabel.Text = "Desviación estándar:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stdDevLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maxTextBox);
+            this.Controls.Add(this.minTextBox);
+            this.Controls.Add(this.cantidadTextBox);
+            this.Controls.Add(this.experimentosTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox experimentosTextBox;
+        private System.Windows.Forms.TextBox cantidadTextBox;
+        private System.Windows.Forms.TextBox minTextBox;
+        private System.Windows.Forms.TextBox maxTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label stdDevLabel;
+    }
+}
+
---
+++ Tarea3_Montecarlo/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea3_Montecarlo
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int experimentos = int.Parse(experimentosTextBox.Text);
+                int cantidad = int.Parse(cantidadTextBox.Text);
+                int a = int.Parse(minTextBox.Text);
+                int b = int.Parse(maxTextBox.Text);
+
+                double result = Montecarlo(experimentos, cantidad, a, b);
+                resultLabel.Text = $"Promedio Monte Carlo: {result:F2}";
+
+                // Display iterations in DataGridView
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("Iteration", "Iteración");
+                dataGridView1.Columns.Add("Value", "Valor");
+
+                for (int i = 0; i < experimentos; i++)
+                {
+                    for (int j = 0; j < cantidad; j++)
+                    {
+                        int value = random.Next(a, b + 1);
+                        dataGridView1.Rows.Add(i * cantidad + j + 1, value);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Introducir solo números válidos, por favor.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double Montecarlo(int experimentos, int cantidad, int a, int b)
+        {
+            double sum = 0;
+            List<double> values = new List<double>();
+
+            for (int i = 0; i < experimentos; i++)
+            {
+                for (int j = 0; j < cantidad; j++)
+                {
+                    int value = random.Next(a, b + 1);
+                    sum += value;
+                    values.Add(value);
+                }
+            }
+
+            double mean = sum / (experimentos * cantidad);
+
+            // Calculate variance
+            double variance = values.Select(val => Math.Pow(val - mean, 2)).Sum() / (experimentos * cantidad);
+
+            // Standard deviation is the square root of variance
+            double stdDev = Math.Sqrt(variance);
+
+            // Display standard deviation
+            stdDevLabel.Text = $"Desviación estándar: {stdDev:F2}";
+
+            return mean;
+        }
+
+ 
+    }
+}
